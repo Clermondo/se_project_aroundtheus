0f445d217ad3bb7c6abe15ca94d0f2e0f701b49e
Clermondo <Isaac.ErismeC@gmail.com>
2024-02-07 04:55:35 -0500

    Corrrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    title: "Yosemite Valley",
-    link: "https://unsplash.com/photos/a-person-walking-across-a-snow-covered-street--DhozujVOF8",
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
-    title: "Lake Louise",
-    link: "https://unsplash.com/photos/a-person-walking-across-a-snow-covered-street--DhozujVOF8",
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    title: "Bald Mountains",
-    link: "https://unsplash.com/photos/a-person-walking-across-a-snow-covered-street--DhozujVOF8",
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
-    title: "Latemar",
-    link: "https://unsplash.com/photos/a-person-walking-across-a-snow-covered-street--DhozujVOF8",
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
-    title: "Vanoise National Park",
-    link: "https://unsplash.com/photos/a-person-walking-across-a-snow-covered-street--DhozujVOF8",
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    title: "Lago di Braies",
-    link: "https://unsplash.com/photos/a-person-walking-across-a-snow-covered-street--DhozujVOF8",
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
 
